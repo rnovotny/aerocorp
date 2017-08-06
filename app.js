a487fe2f02ca063aa50f780da567f7b389197921
Ryan Novotny <ryan.novotny@gmail.com>
2017-08-06 17:13:53 +0700

    fix layers

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 				var startAirport = $('#' + start ).data('airport')
 				var endAirport = $(this).data('airport')
 				
-				svg.append("path")
+				svg.insert("path", '.airport')
 					.datum({type: "LineString", coordinates: [[startAirport.lon, startAirport.lat], [endAirport.lon, endAirport.lat]]})
 					.attr('class', 'route')
 					.attr('id', startAirport.code + '-' + endAirport.code )
@@ -26,44 +26,45 @@ $(document).ready(function(){
 	var projection = d3.geo.orthographic().scale(300).rotate([0, 0]).translate([width / 2, height / 2]).clipAngle(90)
 	 
 	var path = d3.geo.path().projection(projection)
-	var svg = d3.select("#map").append("svg").attr("width", width ).attr("height", height )
+	var svg = d3.select( '#map' ).append( 'svg' ).attr( 'width', width ).attr( 'height', height )
 
 	d3.json("world-110m2.json", function(error, topology) {
 			  
 		// load cities
 		d3.csv("cities.csv", function( data ) {
 			for ( var i = 0 ; i < data.length; i++ ) {
-				svg.append("path")
-					.datum({type: "Point", coordinates: [data[i].lon, data[i].lat]})
-					.attr('class', 'airport')
-					.attr('title', data[i].code )
-					.attr('id', data[i].code )
-					.attr('data-airport', JSON.stringify( data[i] ) )
-					.attr("d", path)
+				svg.append('path')
+					.datum( {type: 'Point', coordinates: [data[i].lon, data[i].lat]} )
+					.attr( 'class', 'airport' )
+					.attr( 'title', data[i].code )
+					.attr( 'id', data[i].code )
+					.attr( 'data-airport', JSON.stringify( data[i] ) )
+					.attr( 'd', path )
 					
 					.on('click', function(){
-						var airportData = $(this).data('airport')
-						$('#airport-code').val(airportData.code)
-						$('#airport-rank').html(airportData.rank)
-						$('#airport-city').html(airportData.city)
-						$('#airport-pax').html(airportData.passengers)
-						$('#airport-lat').html(airportData.lat)
-						$('#airport-lon').html(airportData.lon)
+						var airportData = $(this).data( 'airport' )
+						$( '#airport-code' ).val( airportData.code )
+						$( '#airport-rank' ).html( airportData.rank )
+						$( '#airport-city' ).html( airportData.city )
+						$( '#airport-pax' ).html( airportData.passengers )
+						$( '#airport-lat' ).html( airportData.lat )
+						$( '#airport-lon' ).html( airportData.lon )
 					})
 			}
 		})
 		
 		// load countries
-		svg.selectAll("path")
+		svg.selectAll( 'path' )
 			.data(topojson.object(topology, topology.objects.countries)
 				.geometries)
 			.enter()
-				.append("path")
-				.attr("d", path)
+				.append( 'path' )
+				.attr( 'class', 'background' )
+				.attr( 'd', path )
 			  
-		svg.call(d3.behavior.zoom().on("zoom", function () {
+		svg.call( d3.behavior.zoom().on("zoom", function () {
 			svg.attr("transform", "scale(" + d3.event.scale + ")")
-			svg.selectAll("path").attr("d", path.pointRadius( 4.5 * ( 1 / d3.event.scale ) ) )
+			svg.selectAll(".airport").attr("d", path.pointRadius( 4.5 * ( 1 / d3.event.scale ) ) )
 		}))
 		
 		svg.call( d3.behavior.drag()
