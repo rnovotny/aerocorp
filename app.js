e2bbb0b6a6740206d50cfcae249a0121da6133e9
Ryan Novotny <ryan.novotny@gmail.com>
2017-11-25 12:35:10 +0700

    add planes & routes

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 /* jshint asi: true */
-$(document).ready(function(){
-	
+
+var width = 600
+var height = 600
+var sens = 0.25
+var projection = d3.geo.orthographic().scale(300).rotate([0, 0]).translate([width / 2, height / 2]).clipAngle(90)
+var path = d3.geo.path().projection(projection)
+var svg = d3.select( '#map' ).append( 'svg' ).attr( 'width', width ).attr( 'height', height )
+var planes = loadPlanes()
+loadMap()
+
+function afterLoad() {
 	$('#new-route').click(function(){
 		var start = $('#airport-code').val()
+		startAirport = $( '#' + start ).data('airport')
+		$('#route-start').html( start )
+		
+		$('.airport').hover( function(){
+			var hoverAirport = $(this).data('airport')
+			$('#route-end').html( hoverAirport.code )
+			var routeDist = haversineDistance( startAirport, hoverAirport )
+			$('#route-distance').html( routeDist )
+			$('#plane-table').removeClass('hasRange')
+		})
 		
 		$('.airport').click(function(){
 			if ( $(this).attr('id') !== start ) {
-				var startAirport = $('#' + start ).data('airport')
 				var endAirport = $(this).data('airport')
-				
 				svg.insert("path", '.airport')
 					.datum({type: "LineString", coordinates: [[startAirport.lon, startAirport.lat], [endAirport.lon, endAirport.lat]]})
 					.attr('class', 'route')
@@ -19,14 +36,26 @@ $(document).ready(function(){
 			}
 		})
 	})
+
+	$('.airport').on('mouseover click', function() {
+		var airportData = $(this).data( 'airport' )
+		$( '#airport-code' ).val( airportData.code )
+		$( '#airport-rank' ).html( airportData.rank )
+		$( '#airport-city' ).html( airportData.city )
+		$( '#airport-pax' ).html( airportData.passengers )
+		$( '#airport-lat' ).html( airportData.lat )
+		$( '#airport-lon' ).html( airportData.lon )
+	})
 	
-	var width = 600
-	var height = 600
-	var sens = 0.25
-	var projection = d3.geo.orthographic().scale(300).rotate([0, 0]).translate([width / 2, height / 2]).clipAngle(90)
-	 
-	var path = d3.geo.path().projection(projection)
-	var svg = d3.select( '#map' ).append( 'svg' ).attr( 'width', width ).attr( 'height', height )
+}
+
+
+
+	
+
+
+
+function loadMap() {
 
 	d3.json("world-110m2.json", function(error, topology) {
 			  
@@ -40,17 +69,8 @@ $(document).ready(function(){
 					.attr( 'id', data[i].code )
 					.attr( 'data-airport', JSON.stringify( data[i] ) )
 					.attr( 'd', path )
-					
-					.on('click', function(){
-						var airportData = $(this).data( 'airport' )
-						$( '#airport-code' ).val( airportData.code )
-						$( '#airport-rank' ).html( airportData.rank )
-						$( '#airport-city' ).html( airportData.city )
-						$( '#airport-pax' ).html( airportData.passengers )
-						$( '#airport-lat' ).html( airportData.lat )
-						$( '#airport-lon' ).html( airportData.lon )
-					})
 			}
+			afterLoad()
 		})
 		
 		// load countries
@@ -75,10 +95,44 @@ $(document).ready(function(){
 				svg.selectAll("path").attr("d", path)
 			})
 		)
-	});
-	
-	
-	
-})
+	})	
+}
 
+function loadPlanes() {
+	var planes = []
+	d3.csv("planes.csv", function( data ) {
+		
+		for ( var i = 0; i < data.length; i++ ) {
+			planes[i] = data[i]
+			var html = '<tr><td>'+data[i].model+'</td><td>'+data[i].price+'</td><td>'+data[i].speed+'</td><td>'+data[i].range+'</td><td>'+data[i].pax+'</td></tr>'
+			$('#planes-table').append( html )
+		}
+	})
+	return planes
+}
 
+function haversineDistance( coords1, coords2 ) {
+	function toRad(x) {
+		return x * Math.PI / 180
+	}
+
+	var lon1 = coords1.lon
+	var lat1 = coords1.lat
+
+	var lon2 = coords2.lon
+	var lat2 = coords2.lat
+
+	var R = 6371 // km
+
+	var x1 = lat2 - lat1
+	var dLat = toRad(x1)
+	var x2 = lon2 - lon1
+	var dLon = toRad(x2)
+	var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+	Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
+	Math.sin(dLon / 2) * Math.sin(dLon / 2);
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+	var d = R * c
+
+	return (d * 0.539957).toFixed(0); //nautical miles
+}
